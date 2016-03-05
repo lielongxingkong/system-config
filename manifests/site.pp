38e5313ca2499a951e6ebab90837436f92966cc6
Zhao Zhenlong <zzl_1164@126.com>
2016-03-05 15:53:15 +0000

    update hiera and site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,12 +22,13 @@ node 'review.incloud-ci.com' {
     ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_chain_file          => '',
 
-    ssh_rsa_key_contents                => hiera('uni_ssh_rsa_key_contents'),
-    ssh_rsa_pubkey_contents             => hiera('uni_ssh_rsa_pubkey_contents'),
-    ssh_project_rsa_key_contents        => hiera('uni_ssh_rsa_key_contents'),
-    ssh_project_rsa_pubkey_contents     => hiera('uni_ssh_rsa_pubkey_contents'),
-    ssh_replication_rsa_key_contents    => hiera('uni_ssh_rsa_key_contents'),
-    ssh_replication_rsa_pubkey_contents => hiera('uni_ssh_rsa_pubkey_contents'),
+    ssh_rsa_key_contents                => hiera('gerrit_ssh_rsa_key_contents'),
+    ssh_rsa_pubkey_contents             => hiera('gerrit_ssh_rsa_pubkey_contents'),
+    ssh_project_rsa_key_contents        => hiera('pm_ssh_rsa_key_contents'),
+    ssh_project_rsa_pubkey_contents     => hiera('pm_ssh_rsa_pubkey_contents'),
+    ssh_replication_rsa_key_contents    => hiera('gerrit_ssh_rsa_key_contents'),
+    ssh_replication_rsa_pubkey_contents => hiera('gerrit_ssh_rsa_pubkey_contents'),
+    ssh_zuul_rsa_pubkey_contents        => hiera('zuul_ssh_rsa_pubkey_contents'),
 
     gitlab_root_passwd      => hiera('gitlab_root_passwd'),
     gitlab_gerrit_passwd    => hiera('gitlab_gerrit_passwd'),
@@ -51,32 +52,31 @@ node 'review.incloud-ci.com' {
 
 node 'singlenode.incloud-ci.com' {
   # If the fqdn is not resolvable, use its ip address
-  $vhost_name = hiera('vhost_name', $::fqdn)
   class { 'openstack_project::server':
     sysadmins                 => hiera('sysadmins', []),
     puppetmaster_server       => 'puppetmaster.incloud-ci.com',
   }
 
   class { '::openstackci::single_node_ci':
-    vhost_name                  => $vhost_name,
+    vhost_name                  => hiera('singlenode_vhost_name'),
     project_config_repo         => hiera('project_config_repo'),
-    serveradmin                 => hiera('serveradmin', "webmaster@${vhost_name}"),
+    serveradmin                 => hiera('serveradmin', "webmaster@incloud-ci.com"),
     jenkins_username            => hiera('jenkins_username', 'jenkins'),
     jenkins_password            => hiera('jenkins_password', 'XXX'),
-    jenkins_ssh_private_key     => hiera('uni_ssh_rsa_key_contents'),
-    jenkins_ssh_public_key      => hiera('uni_ssh_rsa_pubkey_contents'),
-    gerrit_server               => hiera('gerrit_server', 'review.openstack.org'),
+    jenkins_ssh_private_key     => hiera('zuul_ssh_rsa_key_contents'),
+    jenkins_ssh_public_key      => hiera('zuul_ssh_rsa_pubkey_contents'),
+    gerrit_server               => hiera('gerrit_server', 'review.incloud-ci.com'),
     gerrit_user                 => hiera('gerrit_user'),
-    gerrit_user_ssh_public_key  => hiera('uni_ssh_rsa_pubkey_contents'),
-    gerrit_user_ssh_private_key => hiera('uni_ssh_rsa_key_contents'),
+    gerrit_user_ssh_public_key  => hiera('zuul_ssh_rsa_pubkey_contents'),
+    gerrit_user_ssh_private_key => hiera('zuul_ssh_rsa_key_contents'),
     gerrit_ssh_host_key         => hiera('gerrit_ssh_host_key',
-      'review.openstack.org,23.253.232.87,2001:4800:7815:104:3bc3:d7f6:ff03:bf5d b8:3c:72:82:d5:9e:59:43:54:11:ef:93:40:1f:6d:a5'),
+      'review.incloud-ci.com,23.253.232.87,2001:4800:7815:104:3bc3:d7f6:ff03:bf5d b8:3c:72:82:d5:9e:59:43:54:11:ef:93:40:1f:6d:a5'),
     git_email                   => hiera('git_email'),
     git_name                    => hiera('git_name'),
     log_server                  => hiera('log_server'),
     smtp_host                   => hiera('smtp_host', 'localhost'),
-    smtp_default_from           => hiera('smtp_default_from', "zuul@${vhost_name}"),
-    smtp_default_to             => hiera('smtp_default_to', "zuul.reports@${vhost_name}"),
+    smtp_default_from           => hiera('smtp_default_from', "zuul@incloud-ci.com"),
+    smtp_default_to             => hiera('smtp_default_to', "zuul.reports@incloud-ci.com"),
     zuul_revision               => hiera('zuul_revision', 'master'),
     zuul_git_source_repo        => hiera('zuul_git_source_repo',
       'https://git.openstack.org/openstack-infra/zuul'),
